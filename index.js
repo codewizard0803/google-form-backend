3e3d5879056b0465f4412a7153528e28280bceb6
Jupiter83 <superdev83@outlook.com>
2023-06-19 16:42:19 +0200

    fixed: port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 const generateDocFile = require("./routes/api/generateDocFile");
 const deleteDownloadFile = require("./routes/api/deleteDownloadFile");
 
